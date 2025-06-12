940bdfcb6e67f31b24ca141eff09fff0612762db
Windrufe <abdulrehamanshaik350@gmail.com>
2025-06-12 23:17:50 +0530

    Updates

--- shared/config.js
+++ shared/config.js
@@ -6,10 +6,10 @@ const config = {
         : 'https://1911118.github.io/Raji',
     apiBaseUrl: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://fluffy-masks-hunt.loca.lt',
+        : 'https://vast-onions-bathe.loca.lt',
     socketUrl: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://fluffy-masks-hunt.loca.lt',
+        : 'https://vast-onions-bathe.loca.lt',
     
     // API endpoints
     api: {
