b371edbe6279fbe61d75308d54a0769fd1126419
Windrufe <abdulrehamanshaik350@gmail.com>
2025-06-12 23:11:52 +0530

    Updates

--- shared/config.js
+++ shared/config.js
@@ -6,10 +6,10 @@ const config = {
         : 'https://1911118.github.io/Raji',
     apiBaseUrl: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://fruity-eels-join.loca.lt',
+        : 'https://fluffy-masks-hunt.loca.lt',
     socketUrl: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://fruity-eels-join.loca.lt',
+        : 'https://fluffy-masks-hunt.loca.lt',
     
     // API endpoints
     api: {
